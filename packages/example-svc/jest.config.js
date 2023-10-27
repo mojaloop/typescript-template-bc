46c7724daec4ab2938ffcf1627c984f265b04888
Pedro Sousa Barreto <pedrosousabarreto@gmail.com>
2023-10-27 11:25:56 +0100

    [skip ci] updated jest to use hierarchy and .gitignore to ignore .idea

--- packages/example-svc/jest.config.js
+++ packages/example-svc/jest.config.js
@@ -1,24 +1,18 @@
 "use strict";
 
-const { name } = require("./package.json");
+const {name} = require("./package.json");
 let packageName = name.replace("@mojaloop", "") || "unknown_package_name";
+const sharedConfig = require("../../jest.config.js");
 
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/test/unit/**/*.test.ts"],
-  passWithNoTests: true,
-  collectCoverage: true,
-  collectCoverageFrom: ["./src/**/*.ts"],
-  coveragePathIgnorePatterns: ["./src/tmp_files"],
-  coverageReporters: ["text", ["json", {file: `../../../coverage/${packageName}-final.json`}]],
-  clearMocks: true,
-  coverageThreshold: {
-    "global": {
-      "branches": 90,
-      "functions": 90,
-      "lines": 90,
-      "statements": -10
-    }
-  }
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["**/test/unit/**/*.test.ts"],
+    passWithNoTests: true,
+    collectCoverage: true,
+    collectCoverageFrom: ["./src/**/*.ts"],
+    coveragePathIgnorePatterns: ["./src/tmp_files"],
+    coverageReporters: ["text", ["json", {file: `../../../coverage/${packageName}-final.json`}]],
+    clearMocks: true,
+    coverageThreshold: sharedConfig.coverageThreshold
 }
